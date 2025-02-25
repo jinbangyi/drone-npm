edf9946d15a80d55c4e36919795d3548a13e6ab7
benny <b1623546466@gmail.com>
2025-02-25 14:47:06 +0800

    debug: add log2

---
+++ build.sh
@@ -0,0 +1,10 @@
+VERSION=$1
+
+export GOOS=linux
+export GOARCH=amd64
+export CGO_ENABLED=0
+export GO111MODULE=on
+
+go build -v -a -tags netgo -o release/linux/amd64/drone-npm
+docker build   --label org.label-schema.build-date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")   --label org.label-schema.vcs-ref=$
+(git rev-parse --short HEAD)   --file docker/Dockerfile.linux.amd64 --tag jinbangyi/plugins-npm:$VERSION .
